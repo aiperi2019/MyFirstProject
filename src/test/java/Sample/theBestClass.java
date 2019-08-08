fc2b9e6e6c0971754c516ad97802ed43a3cd65be
aiperizh <aiperizh@gmail.com>
2019-08-07 22:02:20 -0500

    kjgc

--- src/test/java/Sample/theBestClass.java
+++ src/test/java/Sample/theBestClass.java
@@ -4,5 +4,6 @@ public class theBestClass {
     public static void main(String[] args) {
         System.out.println("this is my code");
         System.out.println("This is my 2nd line of code without any conflicts");
+        System.out.println("Not really without conflict, PS MArkovka");
     }
 }
